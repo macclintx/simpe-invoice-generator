58e5e320fb8b73b6854df1b6c0951534b2bc93d1
macclintx <macclintx@gmail.com>
2025-04-02 01:33:02 +0300

    defining variables required

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    define('TAX_RATE', 16); //16% VAT
+    define('DISCOUNT_THRESHOLD', 80000); //discount applies if total exceeds 80000
+    define('DISCOUNT_RATE', 10); //10% discount
+
+    $item1_name = 'Laptop';
+    $item1_price = 75000;
+    $item1_quantity = 1;
+
+    $item2_name = 'Mouse';
+    $item2_price = 1500;
+    $item2_quantity = 2;
+
+    $item3_name = 'Keyboard';
+    $item3_price = 3500;
+    $item3_quantity = 1;
+
+
+    function calculateTotal($item_price, $item_quanity){
+        
+    }

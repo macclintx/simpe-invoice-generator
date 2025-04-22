3d5e28867f58b9b0c7adac968cffa8bfa70b5a1c
macclintx <macclintx@gmail.com>
2025-04-23 02:15:34 +0300

    including selection options

--- index.view.php
+++ index.view.php
@@ -6,6 +6,21 @@
         <title>Invoice Generator</title>
     </head>
     <body>
+        <form action="index.php" method="post">
         
+        <label for="laptop">Laptop</label>
+        <input type="checkbox" name="laptop" id="laptop" value="laptop">
+        <input type="number" name="laptop_quantity" id="laptop_quantity" placeholder="Laptop Quantity" min="0" required><br>            
+
+        <label for="mouse">Mouse</label>
+        <input type="checkbox" name="mouse" id="mouse" value="mouse">
+        <input type="number" name="mouse_quantity" id="mouse_quantity" placeholder="Mouse Quantity" min="0" required><br>
+
+        <label for="keyboard">Keyboard</label>
+        <input type="checkbox" name="keyboard" id="keyboard" value="keyboard">
+        <input type="number" name="keyboard_quantity" id="keyboard_quantity" placeholder="Keyboard Quantity" min="0" required><br>
+        
+            <input type="submit" value="Generate invoice">
+        </form>
     </body>
 </html>
